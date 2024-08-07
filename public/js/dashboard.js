caa968cead86c5bb0c542b41ec9c26f647d4aaf9
njabdullah <abdullahnj50@gmail.com>
2024-08-07 13:59:18 +0700

    using global time

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,31 +2,6 @@
 // ---------------------------------------------------------------------------------------- ATUR DATE AND TIME ----------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// var initialTime = new Date();
-// var dateElement = document.getElementById('date');
-// var timeElement = document.getElementById('time');
-
-// function updateDateTime() {
-//     var now = new Date();
-//     var timeSinceStart = now.getTime() - initialTime.getTime();
-//     var adjustedTime = new Date(initialTime.getTime() + timeSinceStart);
-
-//     var date = adjustedTime.getFullYear() + '-' +
-//                ('0' + (adjustedTime.getMonth() + 1)).slice(-2) + '-' +
-//                ('0' + adjustedTime.getDate()).slice(-2);
-//     var time = ('0' + adjustedTime.getHours()).slice(-2) + ':' +
-//                ('0' + adjustedTime.getMinutes()).slice(-2) + ':' +
-//                ('0' + adjustedTime.getSeconds()).slice(-2);
-
-//     dateElement.textContent = date;
-//     timeElement.textContent = time;
-
-//     updateMachineDetails(adjustedTime, date, time);
-// }
-
-// setInterval(updateDateTime, 1000);
-
-var startTime = new Date('2024-07-02T20:59:55');
 var initialTime = new Date();
 var dateElement = document.getElementById('date');
 var timeElement = document.getElementById('time');
@@ -34,7 +9,7 @@ var timeElement = document.getElementById('time');
 function updateDateTime() {
     var now = new Date();
     var timeSinceStart = now.getTime() - initialTime.getTime();
-    var adjustedTime = new Date(startTime.getTime() + timeSinceStart);
+    var adjustedTime = new Date(initialTime.getTime() + timeSinceStart);
 
     var date = adjustedTime.getFullYear() + '-' +
                ('0' + (adjustedTime.getMonth() + 1)).slice(-2) + '-' +
@@ -50,6 +25,31 @@ function updateDateTime() {
 }
 
 setInterval(updateDateTime, 1000);
+
+// var startTime = new Date('2024-07-02T20:59:55');
+// var initialTime = new Date();
+// var dateElement = document.getElementById('date');
+// var timeElement = document.getElementById('time');
+
+// function updateDateTime() {
+//     var now = new Date();
+//     var timeSinceStart = now.getTime() - initialTime.getTime();
+//     var adjustedTime = new Date(startTime.getTime() + timeSinceStart);
+
+//     var date = adjustedTime.getFullYear() + '-' +
+//                ('0' + (adjustedTime.getMonth() + 1)).slice(-2) + '-' +
+//                ('0' + adjustedTime.getDate()).slice(-2);
+//     var time = ('0' + adjustedTime.getHours()).slice(-2) + ':' +
+//                ('0' + adjustedTime.getMinutes()).slice(-2) + ':' +
+//                ('0' + adjustedTime.getSeconds()).slice(-2);
+
+//     dateElement.textContent = date;
+//     timeElement.textContent = time;
+
+//     updateMachineDetails(adjustedTime, date, time);
+// }
+
+// setInterval(updateDateTime, 1000);
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------- ATUR MACHINE INFORMATION (LINE, LINEDESC, SHIFT) -------------------------------------------------------------------------
